25126a1641edd9326d638c63450d90486f05d2bd
BramMN <Brammerten@live.nl>
2022-11-04 13:01:05 +0100

    navigating programmatically

--- next-pages-routing/pages/clients/[id]/index.js
+++ next-pages-routing/pages/clients/[id]/index.js
@@ -4,9 +4,18 @@ function ClientProjectsPage() {
   const router = useRouter()
 
   console.log(router.query)
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "bram", clientprojectid: "projecta" }
+    })
+  }
+  
   return (
     <div>
       <h1>The Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   )
 }
