b3401d2471823a78152e17ea7dbff033fb3c3540
BramMN <Brammerten@live.nl>
2022-11-04 12:52:02 +0100

    navigating dynamically

--- next-pages-routing/pages/clients/index.js
+++ next-pages-routing/pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link"
+
 function ClientsPage() {
+  const clients = [
+    { id: "bram", name: "Bram" },
+    { id: "manu", name: "Manuel" },
+  ]
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map(client => (
+          <li key={clients.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
