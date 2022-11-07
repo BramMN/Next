a6b5114337436119350cd779f6a9fca2507aef90
BramMN <Brammerten@live.nl>
2022-11-07 15:16:07 +0100

    getserversideprops code

--- next-data-fetching/pages/user-profile.js
+++ next-data-fetching/pages/user-profile.js
@@ -7,8 +7,6 @@ export default UserProfilePage
 export async function getServerSideProps(context) {
   const { params, req, res } = context
 
-  console.log(req, res)
-  
   return {
     props: {
       username: "Bram",
