59ae5b70eebf927894cf6b9ff64049884ef7fdf2
BramMN <Brammerten@live.nl>
2022-11-08 10:37:50 +0100

    SSG on all events page

--- next-routing-project/pages/events/index.js
+++ next-routing-project/pages/events/index.js
@@ -1,16 +1,16 @@
-import { getAllEvents } from "../../dummy_data"
 import EventList from "../../components/events/event-list"
 import EventsSearch from "../../components/events/events-search"
 import { Fragment } from "react"
 import { useRouter } from "next/router"
+import { getAllEvents } from "../../helpers/api-util"
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter()
-  const events = getAllEvents()
+  const { events } = props
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`
-    
+
     router.push(fullPath)
   }
 
@@ -20,6 +20,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents()
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  }
 }
 
 export default AllEventsPage
