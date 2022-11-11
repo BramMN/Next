4898f71e9de83e7454028222361f154b81a16736
BramMN <Brammerten@live.nl>
2022-11-11 10:46:14 +0100

    adding a newsletter route

--- next-routing-project/components/input/newsletter-registration.js
+++ next-routing-project/components/input/newsletter-registration.js
@@ -1,12 +1,21 @@
-import classes from './newsletter-registration.module.css';
+import { useRef } from "react"
+import classes from "./newsletter-registration.module.css"
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+  const emailInputRef = useRef()
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+  function registrationHandler(event) {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value
+
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
   }
 
   return (
@@ -14,17 +23,12 @@ function NewsletterRegistration() {
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
+          <input type="email" id="email" placeholder="Your email" aria-label="Your email" ref={emailInputRef} />
           <button>Register</button>
         </div>
       </form>
     </section>
-  );
+  )
 }
 
-export default NewsletterRegistration;
+export default NewsletterRegistration
