3e0274a36ef1bb0e79309976b5e22857c360c7bd
BramMN <Brammerten@live.nl>
2022-11-11 12:44:02 +0100

    using context data in components

--- next-routing-project/pages/_app.js
+++ next-routing-project/pages/_app.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import Layout from "../components/layout/layout"
-import Notification from "../components/ui/notification"
 import { NotificationContextProvider } from "../store/notification-context"
 import "../styles/globals.css"
 
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps }) {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Component {...pageProps} />
-        <Notification title="Test" message="this is a test" status="pending" />
       </Layout>
     </NotificationContextProvider>
   )
