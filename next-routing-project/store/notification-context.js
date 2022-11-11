b66ea26c475fc6524338c142c84c3185af7db96d
BramMN <Brammerten@live.nl>
2022-11-11 12:39:49 +0100

    adding context state

--- next-routing-project/store/notification-context.js
+++ next-routing-project/store/notification-context.js
@@ -1,13 +1,29 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 })
 
 export function NotificationContextProvider(props) {
-  return <NotificationContext.Provider>{props.children}</NotificationContext.Provider>
+  const [activeNotification, setActiveNotification] = useState()
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData)
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null)
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  }
+
+  return <NotificationContext.Provider value={context}>{props.children}</NotificationContext.Provider>
 }
 
 export default NotificationContext
