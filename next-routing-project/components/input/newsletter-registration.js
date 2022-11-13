9038a60e24ca6208303bc5aa07b7737d2a343b87
BramMN <Brammerten@live.nl>
2022-11-13 12:37:36 +0100

    triggering and showing notifications

--- next-routing-project/components/input/newsletter-registration.js
+++ next-routing-project/components/input/newsletter-registration.js
@@ -1,7 +1,10 @@
-import { useRef } from "react"
+import { useContext, useRef } from "react"
+import NotificationContext from "../../store/notification-context"
 import classes from "./newsletter-registration.module.css"
 
 function NewsletterRegistration() {
+  const notificationCtx = useContext(NotificationContext)
+
   const emailInputRef = useRef()
 
   function registrationHandler(event) {
@@ -9,13 +12,39 @@ function NewsletterRegistration() {
 
     const enteredEmail = emailInputRef.current.value
 
+    notificationCtx.showNotification({
+      title: "Signing Up...",
+      message: "Registering for newsletter.",
+    })
+
     fetch("/api/newsletter", {
       method: "POST",
       body: JSON.stringify({ email: enteredEmail }),
       headers: { "Content-Type": "application/json" },
     })
-      .then(response => response.json())
-      .then(data => console.log(data))
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message || "Something went wrong!")
+        })
+      })
+      .then(data => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Successfully registered for newsletter!",
+          status: "success",
+        })
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        })
+      })
   }
 
   return (
