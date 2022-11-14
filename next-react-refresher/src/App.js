bdc30f8bde7a02d9f2fc4649e30cfa1c58762d05
BramMN <Brammerten@live.nl>
2022-11-14 12:19:21 +0100

    outputting lists of data

--- next-react-refresher/src/App.js
+++ next-react-refresher/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
-        <Route path="/new-meetup">
+        <Route path="/new-meetups">
           <NewMeetupsPage />
         </Route>
         <Route path="/favorites">
