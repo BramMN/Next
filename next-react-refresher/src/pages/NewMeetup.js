db24a8608b17a72860325f5dcddaaa820498b552
BramMN <Brammerten@live.nl>
2022-11-15 10:44:58 +0100

    navigating programmatically

--- next-react-refresher/src/pages/NewMeetup.js
+++ next-react-refresher/src/pages/NewMeetup.js
@@ -1,11 +1,16 @@
+import { useHistory } from "react-router-dom"
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupsPage() {
+  const history = useHistory()
+  
   function addMeetupHandler(meetupData) {
     fetch(process.env.REACT_APP_FIREBASE + "meetups.json", {
       method: "POST",
       body: JSON.stringify(meetupData),
       headers: { "Content-Type": "application/json" },
+    }).then(() => {
+      history.replace("/")
     })
   }
 
