f00b32add4f7c5cce76e4906b60284f306012d51
BramMN <Brammerten@live.nl>
2022-11-15 10:41:04 +0100

    adding post http request

--- next-react-refresher/src/pages/NewMeetup.js
+++ next-react-refresher/src/pages/NewMeetup.js
@@ -2,9 +2,13 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupsPage() {
   function addMeetupHandler(meetupData) {
-    
+    fetch(process.env.REACT_APP_FIREBASE + "meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: { "Content-Type": "application/json" },
+    })
   }
-  
+
   return (
     <section>
       <h1>Add New Meetup</h1>
