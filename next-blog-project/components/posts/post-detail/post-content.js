416a7ed0798f360fc08070b8c66f12041670ed17
BramMN <Brammerten@live.nl>
2022-11-17 12:32:18 +0100

    rendering code snippets with markdown

--- next-blog-project/components/posts/post-detail/post-content.js
+++ next-blog-project/components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown"
 import PostHeader from "./post-header"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
 import classes from "./post-content.module.css"
 import Image from "next/image"
@@ -27,6 +29,17 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>
+    },
+
+    code(code) {
+      const { className, children } = code
+      const language = className.split("-")[1]
+
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      )
     },
   }
 
