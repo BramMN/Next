f2e0ba13cadff28a2da9ca00aebfe0559cb20cff
BramMN <Brammerten@live.nl>
2022-11-18 12:47:12 +0100

    changing next config options

--- next-blog-project/next.config.js
+++ next-blog-project/next.config.js
@@ -1,7 +1,30 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      swcMinify: true,
+      env: {
+        mongodb_userName: "dbUser",
+        mongodb_password: "",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    }
+  }
+  
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+      mongodb_userName: "dbUser",
+      mongodb_password: "",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  }
 }
 
 module.exports = nextConfig
