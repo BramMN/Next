036809f856c0896ac51519d609fb1ac5a94bb9ea
BramMN <Brammerten@live.nl>
2022-11-20 15:42:53 +0100

    adding client side page guards

--- next-authentication/components/profile/user-profile.js
+++ next-authentication/components/profile/user-profile.js
@@ -1,8 +1,24 @@
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 import ProfileForm from "./profile-form"
 import classes from "./user-profile.module.css"
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!session || status === "unauthenticated") {
+      router.push("/auth")
+    }
+  }, [router, session, status])
+
+  if (!session || loading) {
+    return <p className={classes.profile}>Loading...</p>
+  }
 
   return (
     <section className={classes.profile}>
