5adf8de61e817dede4fcc28362b379022b9f4133
BramMN <Brammerten@live.nl>
2022-11-20 16:58:28 +0100

    adding the change password logic

--- next-authentication/pages/api/user/change-password.js
+++ next-authentication/pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from "next-auth/react"
+import { hashPassword, verifyPassword } from "../../../lib/auth"
+import { connectToDatabase } from "../../../lib/db"
 
 async function handler(req, res) {
   if (req.method !== "PATCH") {
@@ -11,6 +13,39 @@ async function handler(req, res) {
     res.status(401).json({ message: "Not Authenticated!" })
     return
   }
+
+  const userEmail = session.user.email
+  const oldPassword = req.body.oldPassword
+  const newPassword = req.body.newPassword
+
+  const client = await connectToDatabase()
+
+  const usersCollection = client.db().collection("users")
+
+  const user = await usersCollection.fineOne({ email: userEmail })
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." })
+    client.close()
+    return
+  }
+
+  const currentPassword = user.password
+
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword)
+
+  if (!passwordAreEqual) {
+    res.status(403).json({ message: "Invalid Password." })
+    client.close()
+    return
+  }
+
+  const hashedNewPassword = await hashPassword(newPassword)
+
+  const result = await usersCollection.updateOne({ email: userEmail }, { $set: { password: hashedNewPassword } })
+
+  client.close()
+  res.status(200).json({ message: "Password Updated! " })
 }
 
 export default handler
