1291d8bcc7158565281da8a885a546dd99bdd42b
BramMN <Brammerten@live.nl>
2022-11-21 11:59:11 +0100

    pages setup

--- next-summary/components/meetups/MeetupList.js
+++ next-summary/components/meetups/MeetupList.js
@@ -1,20 +1,14 @@
-import MeetupItem from './MeetupItem';
-import classes from './MeetupList.module.css';
+import MeetupItem from "./MeetupItem"
+import classes from "./MeetupList.module.css"
 
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
+      {props.meetups.map(meetup => (
+        <MeetupItem key={meetup.id} id={meetup.id} image={meetup.image} title={meetup.title} address={meetup.address} />
       ))}
     </ul>
-  );
+  )
 }
 
-export default MeetupList;
+export default MeetupList
