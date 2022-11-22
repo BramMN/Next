9a4f930f343666561109637a6ce6e04cc0b43d3d
BramMN <Brammerten@live.nl>
2022-11-22 13:25:41 +0100

    switch fallback to blocking

--- next-summary/pages/[meetupId]/index.js
+++ next-summary/pages/[meetupId]/index.js
@@ -62,7 +62,7 @@ export async function getStaticPaths() {
 
   return {
     paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } })),
-    fallback: false,
+    fallback: "blocking",
   }
 }
 
