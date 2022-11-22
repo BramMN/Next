f5460d617fd399d9876bfaee7edb974cecbde7e7
BramMN <Brammerten@live.nl>
2022-11-22 12:29:59 +0100

    connecting and querying to a mongodb

--- next-summary/pages/api/new-meetup.js
+++ next-summary/pages/api/new-meetup.js
@@ -1,10 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb"
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body
 
-    const { title, image, address, description } = data
+    const client = await MongoClient.connect(env.process.NEXT_PUBLIC_MONGODB)
 
-    
+    const db = client.db()
+
+    const meetupsCollection = db.collection("meetups")
+
+    const result = await meetupsCollection.insertOne(data)
+
+    client.close()
+
+    console.log(result)
+    res.status(201).json({ message: "Meetup inserted!" })
   }
 }
 
