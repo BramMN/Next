5315830915e9f8ca16bd04b003c5e9a546a12443
BramMN <Brammerten@live.nl>
2022-11-22 12:12:53 +0100

    getStaticProps and getStaticPaths for data importing

--- next-summary/pages/[meetupId]/index.js
+++ next-summary/pages/[meetupId]/index.js
@@ -12,4 +12,38 @@ function MeetupDetails() {
   )
 }
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: false,
+  }
+}
+
 export default MeetupDetails
